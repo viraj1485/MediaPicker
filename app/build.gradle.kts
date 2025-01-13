9b1eff9e3fb2e446b70fa7f4a4aa3c1c16081a4f
VirajBenade <viraj.b@reapmind.com>
2025-01-13 11:14:53 +0530

    2nd commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,6 @@ android {
 
 dependencies {
 
-    implementation(project(":mediapicker"))
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
